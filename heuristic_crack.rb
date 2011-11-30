f1e67c103ea0fdb6bd41f5f7594ad604c8e01419
Brandon Smith <brandon.smith@studiobebop.net>
2011-11-29 18:47:54 -0600

    now you can see lots of letters for no reason.

--- heuristic_crack.rb
+++ heuristic_crack.rb
@@ -6,9 +6,12 @@ Slop.parse :help => true do
     banner "heuristic_crack [options]"
 
     on "valid-words", "Returns all valid English-like words and phrases up to 4 characters long" do
-        b = GentleBrute::BruteForcer
+        max_length = 4
+        b = GentleBrute::BruteForcer.new
         while true
-            puts b.next_valid_phrase
+            phrase = b.next_valid_phrase
+            break if phrase.length > max_length
+            puts phrase
         end
     end
 
--- lib/gentle_brute.rb
+++ lib/gentle_brute.rb
@@ -4,9 +4,7 @@ Encoding.default_internal = Encoding.find 'UTF-8'
 require 'bundler'
 Bundler.require
 
-puts __FILE__
-puts File.expand_path("../gentle_brute", __FILE__)
-$:.unshift File.expand_path("../gentle_brute", __FILE__)
+$:.unshift File.expand_path("..", __FILE__)
 require 'gentle_brute/pattern_finder'
 require 'gentle_brute/word_analyzer'
 require 'gentle_brute/odometer'
@@ -17,7 +15,7 @@ module GentleBrute
     # @param [Number] cpa_threshold the minimum character position analysis score neighboring characters must have
     def initialize(start_length=1, cpa_threshold=25)
       @odometer = Odometer.new start_length
-      @word_analyser = WordAnalyzer.new cpa_threshold
+      @word_analyzer = WordAnalyzer.new cpa_threshold
     end
 
     # @return [String] the next English-like brute force phrase
